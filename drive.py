296362d2b8c422ccfb777d715a82504b66b79225
kieranreillyuwa <lpcunningham99@gmail.com>
2022-05-08 17:49:17 +0800

    created drive and waypoint nodes

--- drive.py
+++ drive.py
@@ -1,1 +1,30 @@
-import eye
+#!/usr/bin/env/python
+import rospy
+
+
+from std_msgs.msg import String
+
+#Waypoint Callback
+def wp_cb(data):
+    msg  = "caller_id: {0}\tdata: {1}".format(rospy.get_caller_id(), data.data)
+    rospy.loginfo(String(msg))
+
+def pos_cb(data):
+    msg  = "caller_id: {0}\tdata: {1}".format(rospy.get_caller_id(), data.data)
+    rospy.loginfo(String(msg))
+
+    
+
+def main():
+    
+    #create node called waypoints
+    rospy.init_node('drive')
+
+    rospy.Subscriber('waypoints_comm', String, wp_cb)
+    rospy.Subscriber('positions_comm', String, pos_cb)
+
+    #sleep until shutdown
+    rospy.spin()
+
+if __name__ -- "__main__":
+    main()
---
+++ waypoints.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env/python
+import rospy
+
+from std_msgs.msg import String
+
+
+def get_waypoints():
+    x,y = 0,0
+    return (x,y)
+
+def publish_waypoints(pub, waypoint):
+    """
+    args:   publisher == node publisher
+            waypoint == list of a coord, waypoint[0] is x and waypoint[1] is y
+    """
+    waypoint_str = String("waypoint: x:{0}, y:{1}".format(waypoint[0], waypoint[1]))
+    rospy.loginfo(waypoint_str)
+    pub.publish(waypoint_str)
+    
+    
+
+def main():
+    
+    pub = rospy.Publisher('waypoints_comm', String, queue_size=10)
+
+    #create node called waypoints
+    rospy.init_node('waypoint')
+
+    #loop every 1/10 seconds
+    rate = rospy.Rate(10)
+
+
+    while not rospy.is_shutdown():
+        #callbacks here
+        curr_waypoint = get_waypoints()
+        publish_waypoints(pub, curr_waypoint)
+
+        rate.sleep()
+        #sleep until shutdown
+        
+
+
+
+if __name__ -- "__main__":
+    main()
